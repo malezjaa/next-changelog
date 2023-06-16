5ff4d23486253403d67438a15d7b0f011e8014f1
BuildTools <poreba.kris@gmail.com>
2023-06-16 17:25:18 +0200

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,56 +1,57 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import {Metadata} from "next";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Metadata } from "next";
 import Navbar from "@/components/Navbar";
-import {getBaseUrl} from "@/utils/urls";
+import { getBaseUrl } from "@/utils/urls";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseUrl()),
   title: {
-    default: 'Next.js Changelog',
-    template: '%s | Next.js Changelog',
+    default: "Next.js Changelog",
+    template: "%s | Next.js Changelog",
   },
-  description: 'The latest updates to the Next.js framework',
+  description: "The latest updates to the Next.js framework",
   alternates: {
     canonical: getBaseUrl(),
   },
   openGraph: {
     title: {
-      default: 'Next.js Changelog',
-      template: '%s | Next.js Changelog',
+      default: "Next.js Changelog",
+      template: "%s | Next.js Changelog",
     },
-    description: 'The latest updates to the Next.js framework',
-    type: 'website',
+    description: "The latest updates to the Next.js framework",
+    type: "website",
     url: getBaseUrl(),
     images: [
-        `${getBaseUrl()}siteimage.png`
-    ]
+      {
+        url: `${getBaseUrl()}siteimage.png`,
+        width: 1858,
+        height: 931,
+      },
+    ],
   },
   twitter: {
-    card: 'summary',
-    title: 'Next.js Changelog',
-    description: 'The latest updates to the Next.js framework',
-    images: [
-      `${getBaseUrl()}siteimage.png`
-    ]
+    card: "summary",
+    title: "Next.js Changelog",
+    description: "The latest updates to the Next.js framework",
+    images: [`${getBaseUrl()}siteimage.png`],
   },
-}
-
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={`${inter.className}`}>
-          <Navbar/>
+        <Navbar />
 
-          <div className="w-full">{children}</div>
+        <div className="w-full">{children}</div>
       </body>
     </html>
-  )
+  );
 }
